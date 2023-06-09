a941b3f50bd023eae2c0a0f5de21023d96fcc09f
Pauline Thiele <Fenstersteuerungber SupportPauline.Thiele@Student.HTW-Berlin.de>
2023-06-09 10:22:19 +0200

    added comments

--- api/index.js
+++ api/index.js
@@ -1,39 +1,57 @@
+// create new websocket server 
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({ port: 7071 });
+
+// create map to store clients metadata (ID, Farbe) --> like a dictionary
 const clients = new Map();
 
-const allMessagesArray = [];
+const allMessagesArray = []; // was zur hölle tut es??
 
-wss.on('connection', (ws) => {
-    const id = uuidv4();
-    const color = Math.floor(Math.random() * 360);
-    const metadata = { id, color };
+// Verbindung eines neuen Clients mit dem Websocketserver & Speicherung der Metadaten des Clients 
+wss.on('connection', (ws, req) => {
+    const id = uuidv4();                              //generieren einer individuellen ID
+    const color = Math.floor(Math.random() * 360);    // Zahl zwischen 0 & 1 --> multipliziert mit 360 --> Farbe auf dem HSV Farbraum 
+    const metadata = { id, color, ip: req.socket.remoteAddress };                   // object mit ID und color 
 
-    clients.set(ws, metadata);
+    clients.set(ws, metadata);      //add new client to map
     allMessagesArray.forEach(message => ws.send(JSON.stringify(message)));
 
-    ws.on('message', (messageAsString) => {
-      const message = JSON.parse(messageAsString);
-      const metadata = clients.get(ws);
+    ws.on('message', (messageAsString) => {           //wird aufgerufen, wenn der Server eine Nachricht emfängt 
+      const message = JSON.parse(messageAsString);    //Nachricht aus Json Datei auslesen 
+      const metadata = clients.get(ws);               //Metadaten des Clients aus Map rauslesen
 
+      // die beiden Inhalte (ID & color) des Clients an die Nachricht hängen 
       message.sender = metadata.id;
       message.color = metadata.color;
-      
+      console.log(message);
+
       allMessagesArray.push(message);
 
+      // an alle anderen verbundenen Clients wird die Nachricht geschickt
       [...clients.keys()].forEach((client) => {
         client.send(JSON.stringify(message));
       });
+
+      ws.on('close', () => {
+        clients.delete(ws);
+        console.log("Client with IP address "+ metadata.ip +" has left the session.");
+
+        //überprüfen ob alle Clients das Browserfenster geschlossen haben 
+        if(clients.size === 0){
+          console.log("Session ended for all clients.");
+          wss.close();
+        }
+      })
     });  
 });
 
-wss.on('close', (ws) => {
-  clients.delete(ws);
-  console.log("user disconnected");
-});
+// wenn Client die Verbindung schließt, alle metadaten dieses Clients von der Map entfernen
+//wss.on("close", (ws) => {
+  //clients.delete(ws);
+  //console.log("user disconnected");
+//});
 
-
+//Funktion zum Erstellen einer individuellen ID 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -42,3 +60,4 @@ function uuidv4() {
 }
 
 console.log("wss up");
+//wss.close();
--- app/index.js
+++ app/index.js
@@ -1,6 +1,40 @@
+// Connect Client to Server 
 (async function() {
-    const ws = await connectToServer();  
 
+    const ws = await connectToServer();    
+
+    // receive Websocket Message
+    ws.onmessage = (webSocketMessage) => {
+        const messageBody = JSON.parse(webSocketMessage.data);
+        const cursor = getOrCreateCursorFor(messageBody);
+        cursor.style.transform = `translate(${messageBody.x}px, ${messageBody.y}px)`;
+        //create textfield through function createtextfield()
+        if(messageBody.val != null)
+        {
+            receiveTextMessage(messageBody);
+        }
+    };      
+    
+    ws.onclose = () => {
+        console.log("closed");
+    };
+    
+    // Cursorbewegung erkennen und durch Json datei an Server senden 
+    document.body.onmousemove = (evt) => {
+        const messageBody = { x: evt.clientX, y: evt.clientY };
+        ws.send(JSON.stringify(messageBody));
+    };
+
+    // es soll nur für mich eine Textbox erscheinen an Stelle x/y
+    // in diese schreibe ich meinen Text
+    // wenn ich enter drücke soll der Text an der Stelle als Textfeld in der richtigen Farbe erscheinen 
+    document.body.onclick = (evt) => { 
+        var X = evt.clientX;
+        var Y = evt.clientY;
+        createInputText(X,Y);
+    };
+
+    //Bestätigungsfunktion für eine Verbindung zwischen Client und Server  
     async function connectToServer() {    
         const ws = new WebSocket('ws://localhost:7071/ws');
         return new Promise((resolve, reject) => {
@@ -13,33 +47,14 @@
         });   
     }
 
-    // Receive Websocket-Messages
-    ws.onmessage = (webSocketMessage) => {
-        const messageBody = JSON.parse(webSocketMessage.data);
-        const cursor = getOrCreateCursorFor(messageBody);
-        cursor.style.transform = `translate(${messageBody.x}px, ${messageBody.y}px)`;
-        // document.getElementById("userCounter").innerText = messageBody.clientCount;
-        if (messageBody.messageInput != null) {
-            const message = receiveMessage(messageBody);
-            message.color = `hsl(${messageBody.color}`;
-        }
-    };
-
-    ws.onclose = () => {
-        console.log("closed");
-    };
-
-    document.body.onmousemove = (evt) => {
-        const messageBody = { x: evt.clientX, y: evt.clientY };
-        ws.send(JSON.stringify(messageBody));
-    };
-
     function getOrCreateCursorFor(messageBody) {
         const sender = messageBody.sender;
         const existing = document.querySelector(`[data-sender='${sender}']`);
         if (existing) {
             return existing;
         }
+        
+        // wenn kein Cursor gefunden wurde, dann Template clonen und mit Daten füllen 
         const template = document.getElementById('cursor');
         const cursor = template.content.firstElementChild.cloneNode(true);
         const svgPath = cursor.getElementsByTagName('path')[0];    
@@ -51,66 +66,54 @@
         return cursor;
     }
 
-    document.body.onclick = (evt) => {
-        const inputX = evt.clientX;
-        const inputY = evt.clientY;
-        if (document.getElementById("inputField") != null) {
-            document.getElementById("inputField").remove();
-        }  
-        createClientInputField(inputX,inputY);
-        // const messageBody = { x: evt.clientX, y: evt.clientY, mouseClick : true };
-    }
+    function sendTextMessage(X, Y, inputfield)
+    {
+        document.getElementById("inputfield").remove();
 
-    function receiveMessage(messageBody){
-        var message = document.createElement("p");
-        message.innerText = messageBody.messageInput;
-        message.style.position = "absolute";
-        message.style.top =  messageBody.messageY;
-        message.style.left =  messageBody.messageX;
-        message.style.fontSize = "32px";
-        message.style.fontFamily = "monospace";
-        message.style.color = `hsl(${messageBody.color},50%,50%)`;
-        // message.style.color = `hsl(${messageBody.color}, 50%, 50%)`;
-        document.body.appendChild(message);
-        // inputField = document.getElementById("inputField");
-        // inputField.remove();
-        return message;  
-    }
-
-    function sendMessage(inputField) {
-        var message = document.createElement("p");
-        message.innerText = inputField.value;
-        message.style.position = "absolute";
-        message.style.top = inputField.style.top;
-        message.style.left = inputField.style.left;
-        document.getElementById("inputField").remove();
-
-        // return message;  
-        const messageBody = { messageX: message.style.left, messageY: message.style.top, messageInput: message.innerText };
+        const messageBody = { x: X, y: Y, val: inputfield };
         ws.send(JSON.stringify(messageBody));
     }
 
-    function createClientInputField(x,y){
-        var inputField = document.createElement("input");
-        inputField.type = "text";
-        inputField.id = "inputField";
-        inputField.style.fontSize = "32px";
-        inputField.style.position = "absolute";
-        inputField.style.fontFamily = "monospace";
-        inputField.style.top = `${y}px`;
-        inputField.style.left = `${x}px`;
-
-        document.body.appendChild(inputField);
-        document.getElementById("inputField").focus(); 
-        inputField.addEventListener("keypress", function(event) {
-            // If the user presses the "Enter" key on the keyboard
-            if (event.key === "Enter") {
-              // Cancel the default action, if needed
-              event.preventDefault();
-              // Trigger the button element with a click
-              sendMessage(inputField, inputField.value);
-            }
-        }); 
+    function receiveTextMessage(messageBody)
+    {
+        const textmessage = document.createElement("p");
+        textmessage.setAttribute("id", "textmessage");
+        textmessage.innerText = messageBody.val;
+        textmessage.style.color = `hsl(${messageBody.color},50%,50%)`;
+        textmessage.style.position = "absolute"; 
+        textmessage.style.fontSize = "large";
+        textmessage.style.left = messageBody.x+"px"; 
+        textmessage.style.top = messageBody.y+"px";
+        document.body.appendChild(textmessage); 
+        
+        return textmessage;
     }
 
+    function createInputText(X,Y)
+    {
+        var inputfield = document.createElement("input");
+        inputfield.setAttribute("type", "text");
+        inputfield.setAttribute("id", "inputfield");
+        inputfield.setAttribute("size", 30);
+        inputfield.setAttribute("placeholder", "enter your text here");
+        inputfield.style.position = "absolute"; 
+
+        document.body.appendChild(inputfield);
+        inputfield.style.left = X+"px";
+        inputfield.style.top = Y+"px";
+        
+        document.getElementById("inputfield").focus();
+
+        inputfield.addEventListener("keypress", function(event){
+            if(event.key === "Enter"){
+                event.preventDefault();
+                sendTextMessage(X, Y, inputfield.value); 
+            }
+        });
+    }
+
+    function leaveChat()
+    {
+        window.close();
+    }
 })();
